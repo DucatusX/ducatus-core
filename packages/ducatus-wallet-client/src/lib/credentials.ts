1b725fcf7d3c1076ac92ce8d67f3d5779f62d188
Dmitriy Kovalyov <dmitriy.kovalyov@ddgcorp.ru>
2019-11-26 11:58:05 +0300

    Changed lib for wallet client

--- packages/ducatus-wallet-client/src/lib/credentials.ts
+++ packages/ducatus-wallet-client/src/lib/credentials.ts
@@ -1,12 +1,12 @@
 'use strict';
 
-import { BitcoreLib } from '../../../crypto-ducatus-wallet-core';
+import { DucatuscoreLib } from '../../../crypto-ducatus-wallet-core';
 
 import { Constants, Utils } from './common';
 const $ = require('preconditions').singleton();
 const _ = require('lodash');
 
-const Bitcore = BitcoreLib;
+const Bitcore = DucatuscoreLib;
 const sjcl = require('sjcl');
 
 export class Credentials {
