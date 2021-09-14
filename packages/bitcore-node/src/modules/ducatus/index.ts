96037307149f9d3a89cf89e347eec0c23259e1dc
Oleg Terlibin <rumblefishinfo@gmail.com>
2021-09-14 17:38:50 +0300

    Added ducx by node (lint)

--- packages/bitcore-node/src/modules/ducatus/index.ts
+++ packages/bitcore-node/src/modules/ducatus/index.ts
@@ -3,10 +3,10 @@ import { BaseModule } from '..';
 export default class DucatusModule extends BaseModule {
   constructor(services: BaseModule['bitcoreServices']) {
     super(services);
-    
+
     services.Libs.register('DUC', '@ducatus/bitcore-lib', '@ducatus/bitcore-p2p');
     services.P2P.register('DUC', services.P2P.get('BTC'));
-    services.CSP.registerService('DUC', services.CSP.get({chain: 'BTC'}));
+    services.CSP.registerService('DUC', services.CSP.get({ chain: 'BTC' }));
     services.Verification.register('DUC', services.Verification.get('BTC'));
   }
 }
--- packages/bitcore-node/src/modules/ducatusx/index.ts
+++ packages/bitcore-node/src/modules/ducatusx/index.ts
@@ -5,7 +5,7 @@ export default class DUCXModule extends BaseModule {
   constructor(services: BaseModule['bitcoreServices']) {
     super(services);
     services.P2P.register('DUCX', services.P2P.get('ETH'));
-    services.CSP.registerService('DUCX', services.CSP.get({chain: 'ETH'}));
+    services.CSP.registerService('DUCX', services.CSP.get({ chain: 'ETH' }));
     services.Api.app.use(DucxRoutes);
     services.Verification.register('DUCX', services.Verification.get('ETH'));
   }
