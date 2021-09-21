3583a686aa93669ed3a19cd669a640afcb8a0c8e
Oleg Terlibin <rumblefishinfo@gmail.com>
2021-09-21 11:01:13 +0300

    duc cwc tx test

--- packages/ducatus-crypto-wallet-core/test/transactions.ts
+++ packages/ducatus-crypto-wallet-core/test/transactions.ts
@@ -534,6 +534,43 @@ describe('Transaction Creation', () => {
       '0200000002ab189f0d3bf494d3307effb79bafe0758907b86621edb8bd4cad4c6c6dc63e64010000006a47304402202eeb967801c0aad4f8241d8f90e2a9e2236f95c189165ba6b2ba4dc6b17bacbe02201b5d4dc0c32f6aa134d93698f85bf4c098d15fcbaada0b6ca2b8076fd8aa2741012102c8f8fa438666cbd287e28fb384b99555e4acce610e8141e887c9c458bba5db5cffffffffab189f0d3bf494d3307effb79bafe0758907b86621edb8bd4cad4c6c6dc63e64000000006a473044022072bdde2c0b413fc42d15d93e768a26f818dc5b225b9359235c09fd0452d6121a022007c00afa396d60d5b7919f2ba31e638817561cab4e2afed7a86dd636ee293c1001210321f2f13aed42db7257b64f77d574071a6e81e460ab3693eefb7482c12d1ff697ffffffff0200e1f505000000001976a914612fb4d5e27a28f5c54018d8948ca3a650741c4188acee152600000000001976a91486823ef7c8e210184cc8675189d37c4c9d8e1e0288ac00000000';
     expect(signed).to.eq(expected);
   });
+
+  it('should create a DUC tx', () => {
+    const recipients = [{ address: 'LtTwAfVc29sqEYCuZdXLY4n9VdSR4Nam42', amount: 1e8 }];
+    const change = 'M2fS19zf4gaT2LyFUkkEsi1CGinuvnbrqx';
+    const utxos = [
+      {
+        mintTxid: '643ec66d6c4cad4cbdb8ed2166b8078975e0af9bb7ff7e30d394f43b0d9f18ab',
+        mintIndex: 1,
+        value: 0.02503422 * 1e8,
+        script: '76a91457884dcfe2ab46d3354a42d97333c95e5b80cf0188ac',
+        address: 'M1rkXAa55AFuEGrfBAvZq3s28L7NsgekBa',
+        sequenceNumber: 4294967294
+      },
+      {
+        mintTxid: '643ec66d6c4cad4cbdb8ed2166b8078975e0af9bb7ff7e30d394f43b0d9f18ab',
+        mintIndex: 0,
+        value: 1e8,
+        script: '76a9144e744a19a009a9dd43a23a7c12045c83e82ac9d288ac',
+        address: 'LtytCSPDDKwT54zLyuT5NCsZXidf3T7Vkk',
+        sequenceNumber: 4294967294
+      }
+    ];
+    const fee = 7440;
+    const tx = Transactions.create({ chain: 'DUC', recipients, change, utxos, fee, rbf: true });
+
+    const keys = [
+      {
+        address: 'LtytCSPDDKwT54zLyuT5NCsZXidf3T7Vkk',
+        privKey: 'f8ee43e1114223e41a15bcde832fbfb636c91a99e92c4d76c5926785cd81f1be'
+      },
+      { address: 'M1rkXAa55AFuEGrfBAvZq3s28L7NsgekBa', privKey: 'bfb29b45424c035c26b4000a2757977216d60ca956444e413d7d9ccea71d22d5' }
+    ];
+    const signed = Transactions.sign({ chain: 'DUC', tx, keys, utxos });
+    const expected =
+      '0200000002ab189f0d3bf494d3307effb79bafe0758907b86621edb8bd4cad4c6c6dc63e640100000000ffffffffab189f0d3bf494d3307effb79bafe0758907b86621edb8bd4cad4c6c6dc63e640000000000ffffffff0200e1f505000000001976a9146ca10f4981053c64efed1dd40c41e4f6e546e07488acee152600000000001976a914c68eb67328a9b88880d5e34d146790feb984817488ac00000000';
+    expect(signed).to.eq(expected);
+  });
 });
 
 describe('Transaction Sign', () => {
